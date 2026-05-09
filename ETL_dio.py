fee7cf3e0ff28fd9fca101d589960d34638f02fc
brenoboaventura321 <breno.boaventura321@gmail.com>
2026-05-08 22:39:40 -0300

    Adiciona arquivos de configuração e dados: .env.example, .gitignore e usuarios_gamelauncher.json

--- ETL_dio.py
+++ ETL_dio.py
@@ -3,3 +3,16 @@ from numpy import append
 import pandas as pd
 import requests
 import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+# Importando os dados
+
+caminho_arquivo_json ='usuarios_gamelauncher.json'
+
+df_json = pd.read_json(caminho_arquivo_json)
+usuarios = df_json.usuarios
+
+
